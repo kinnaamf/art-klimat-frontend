ed302bd2a70a76f6f2eeba8cf65bebe3a9f27940
kinnaamf <dimoje.1337@gmail.com>
2026-06-25 20:34:41 +0300

    feat: add cooperation section

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,24 @@ import SectionCollaboration from "@/components/sections/SectionCollaboration.vue
 import SectionService from "@/components/sections/SectionService.vue";
 import AppFooter from "@/components/AppFooter.vue";
 import SectionProduction from "@/components/sections/SectionProduction.vue";
+import SectionCooperation from "@/components/sections/SectionCooperation.vue";
 </script>
 
 <template>
   <div class="max-w-[1440px] mx-auto">
     <AppHero/>
 
-    <SectionCollaboration class="my-24 px-16"/>
-    <SectionService class="my-24 px-16"/>
-    <SectionProduction class="mt-24 mb-[128px] px-16"/>
+    <SectionCollaboration class="my-24"/>
+    <SectionService class="my-24"/>
+    <SectionProduction class="mt-24 mb-[128px]"/>
+    <SectionCooperation class="my-24"/>
 
     <AppFooter/>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="postcss">
+section {
+  padding: 0 64px;
+}
+</style>
---
+++ src/components/sections/SectionCooperation.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const collaborationList = [
+  {
+    title: 'Строительные компании',
+    count: 85,
+    description: 'Мы предлагаем качественные и надежные вентиляционные изделия по доступным ценам',
+  },
+  {
+    title: 'Инжиниринговые компании',
+    count: 47,
+    description: 'Мы предлагаем качественные и надежные вентиляционные изделия по доступным ценам',
+  },
+  {
+    title: 'Монтажные организации',
+    count: 65,
+    description: 'Мы предлагаем качественные и надежные вентиляционные изделия по доступным ценам',
+  },
+
+]
+</script>
+
+<template>
+<section>
+  <h2 class="text-[4rem] leading-[72px] text-darkgray font-medium">
+    Сферы для <span class="text-orange">сотрудничества</span>
+  </h2>
+
+
+  <div class="mt-12 flex flex-col divide-y divide- divide-darkgray/10 py-10">
+    <div class="h-[0.1px] bg-darkgray/10"></div>
+    <div v-for="item in collaborationList" class="flex justify-between py-10">
+      <div class="flex items-start gap-4">
+        <h3 class="text-[3rem] font-medium leading-14 text-darkgray">{{ item.title }}</h3>
+        <span class="text-darkgray/60 text-base font-normal leading-6">{{ item.count }} проектов</span>
+      </div>
+      <div class="w-[480px]">
+        <h4 class="text-xl text-darkgray/60 -tracking-wide leading-7">{{ item.description }}</h4>
+      </div>
+    </div>
+    <div class="h-[0.1px] bg-darkgray/10"></div>
+
+  </div>
+</section>
+</template>
+
+<style scoped lang="postcss">
+
+</style>
