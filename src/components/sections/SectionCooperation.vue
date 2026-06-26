24966f226e96bcf4ac89dae35dfbe1f1d6088c5b
kinnaamf <dimoje.1337@gmail.com>
2026-06-26 14:50:36 +0300

    feat: edit cooperation section

--- src/components/sections/SectionCooperation.vue
+++ src/components/sections/SectionCooperation.vue
@@ -27,17 +27,21 @@ const collaborationList = [
 
 
   <div class="mt-12 flex flex-col divide-y divide- divide-darkgray/10 py-10">
-    <div class="h-[0.1px] bg-darkgray/10"></div>
-    <div v-for="item in collaborationList" class="flex justify-between py-10">
+    <div class="h-[0.1px] bg-darkgray/5"></div>
+    <div v-for="item in collaborationList" class="flex justify-between gap-8 py-10">
       <div class="flex items-start gap-4">
-        <h3 class="text-[3rem] font-medium leading-14 text-darkgray">{{ item.title }}</h3>
-        <span class="text-darkgray/60 text-base font-normal leading-6">{{ item.count }} проектов</span>
+        <span class="text-[3rem] font-medium leading-14 text-darkgray w-min text-nowrap">
+          {{ item.title }}
+        </span>
+        <span class="text-darkgray/60 text-base font-normal leading-6 pb-3">
+          {{ item.count }} проектов
+        </span>
       </div>
       <div class="w-[480px]">
         <h4 class="text-xl text-darkgray/60 -tracking-wide leading-7">{{ item.description }}</h4>
       </div>
     </div>
-    <div class="h-[0.1px] bg-darkgray/10"></div>
+    <div class="h-[0.1px] bg-darkgray/5"></div>
 
   </div>
 </section>
